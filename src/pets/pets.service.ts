bd3f23db137ec3f7bc3ee18e8751bde13aedd621
Brixter Porras <brixterporras@gmail.com>
2022-12-08 13:24:16 +0800

    initial commit

---
+++ src/pets/pets.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Pet } from './pet.entity';
+
+@Injectable()
+export class PetsService {
+
+
+    async findAll(): Promise<Pet[]>{
+        const pet = new Pet();
+        pet.id = 1;
+        pet.name= "Tom";
+        pet.type ="Cat";
+
+        return [pet];
+    }
+}
