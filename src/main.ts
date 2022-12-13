acfcf6828319ad6c3d3308eaad9917b34f443d19
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2022-12-13 18:10:19 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe())//data validation
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
